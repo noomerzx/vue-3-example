65e87f29fdf58f38456fd49d42e45bce1434d4ba
Wisarut Phuvanantanond <noomerzx@gmail.com>
2020-12-18 16:11:29 +0700

    test dev branch

---
+++ src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <button @click="test">click</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+
+export default defineComponent({
+  name: 'Home',
+  components: {
+    HelloWorld
+  },
+  setup () {
+    function test () {
+      console.log('111')
+    }
+    return {
+      test
+    }
+  }
+})
+</script>
